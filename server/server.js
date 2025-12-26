e9980b5c7cc4c05bd172097d52a6e4cfbce0ba2c
shivam-metablock <shivam@metablocktechnologies.in>
2025-12-26 12:14:38 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: '*', 
+    origin: 'https://instagram-like.onrender.com', 
     credentials: true,
 }));
 app.use(express.json());
