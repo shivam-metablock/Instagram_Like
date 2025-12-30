08da2d4283818d214bba34bbefee26d7daae63d0
shivam-metablock <shivam@metablocktechnologies.in>
2025-12-30 10:27:47 +0530

    update code

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['https://instagram-like-eta.vercel.app','http://localhost:5173'],
+    origin: ['https://chiapmarket.com','http://localhost:5173'],
     credentials: true,
 }));
 app.use(express.json());
